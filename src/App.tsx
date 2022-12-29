98475dd28d9c15da711f57a2ded187808e7d373d
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-12-29 11:08:09 -0300

    feat: rotas, fontes, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Routes"
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/theme/default"
+
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
