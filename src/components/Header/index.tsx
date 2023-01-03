9410058a99c2c27c10d8ca2790f660cea0b7db59
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2023-01-03 17:17:27 -0300

    feat: adicionado nova página e formularios

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,25 @@
 import { HeaderButton, HeaderButtonContainer, Headercontainer } from "./styles";
 import coffeeLogoImg from "../../assets/coffee-delivery-logo.svg"
 import { MapPin, ShoppingCart } from "phosphor-react"
+import { NavLink } from "react-router-dom";
 
 export function Header(){
   return(
     <Headercontainer>
       <div className="container">
-        <img src={coffeeLogoImg} alt="" />
+        <NavLink to="/">
+          <img src={coffeeLogoImg} alt="" />
+        </NavLink>
         <HeaderButtonContainer>
           <HeaderButton variant="purple">
             <MapPin size={20} weight="fill"/>
             Porto Alegre, RS
           </HeaderButton>
-          <HeaderButton variant="yellow">
-            <ShoppingCart size={20} weight="fill"/>
-          </HeaderButton>
+          <NavLink to="/completeOrder">
+            <HeaderButton variant="yellow">
+              <ShoppingCart size={20} weight="fill"/>
+            </HeaderButton>
+          </NavLink>
         </HeaderButtonContainer>
       </div>
     </Headercontainer>
---
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -0,0 +1,35 @@
+import { TitleText } from "../../../../components/Typography";
+import { SectionTitle } from "../SectionTitle";
+import { CompleteOrderFormContainer, FormSectionContainer } from "./styles";
+import { CurrencyDollar, MapPinLine } from "phosphor-react";
+import { size } from "polished";
+import { useTheme } from "styled-components";
+import { AdressForm } from "./AdressForm";
+import { PaymentMethodOptions } from "./PaymentMethodOptions";
+
+export function CompleteOrderForm(){
+  const { colors } = useTheme()
+  return(
+    <CompleteOrderFormContainer>
+      <TitleText size="xs" color="subtitle">
+        Complete seu pedido
+      </TitleText>
+      <FormSectionContainer>
+        <SectionTitle 
+          title="Endereço de Entrega" 
+          subtitle="Informe o endereço onde deseja receber seu pedido"
+          icon={<MapPinLine size={22} color={colors["brand-yellow-dark"]}/>}
+        />
+        <AdressForm/>
+      </FormSectionContainer>
+      <FormSectionContainer>
+        <SectionTitle 
+          title="Pagamento" 
+          subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
+          icon={<CurrencyDollar size={22} color={colors["brand-purple"]}/>}
+        />
+        <PaymentMethodOptions/>
+      </FormSectionContainer>
+    </CompleteOrderFormContainer>
+  );
+}
